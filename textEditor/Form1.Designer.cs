ab474d5a82c3ae024e5a78924c9ce1b52708d26a
SseBaaa <89750702+SseBaaa@users.noreply.github.com>
2022-05-03 13:07:52 +0200

    Add files via upload

---
+++ textEditor/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace textEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbUpis = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnUnder = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbUpis
+            // 
+            this.rtbUpis.Location = new System.Drawing.Point(22, 70);
+            this.rtbUpis.Name = "rtbUpis";
+            this.rtbUpis.Size = new System.Drawing.Size(889, 564);
+            this.rtbUpis.TabIndex = 0;
+            this.rtbUpis.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("MV Boli", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(981, 422);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(262, 90);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("MV Boli", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(981, 533);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(262, 90);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBold
+            // 
+            this.btnBold.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBold.Location = new System.Drawing.Point(37, 13);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(89, 51);
+            this.btnBold.TabIndex = 3;
+            this.btnBold.Text = "Bold";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnUnder
+            // 
+            this.btnUnder.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnder.Location = new System.Drawing.Point(132, 12);
+            this.btnUnder.Name = "btnUnder";
+            this.btnUnder.Size = new System.Drawing.Size(120, 51);
+            this.btnUnder.TabIndex = 4;
+            this.btnUnder.Text = "Underline";
+            this.btnUnder.UseVisualStyleBackColor = true;
+            this.btnUnder.Click += new System.EventHandler(this.btnUnder_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItalic.Location = new System.Drawing.Point(258, 12);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(89, 51);
+            this.btnItalic.TabIndex = 5;
+            this.btnItalic.Text = "Italic";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1251, 646);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnUnder);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.rtbUpis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbUpis;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnUnder;
+        private System.Windows.Forms.Button btnItalic;
+    }
+}
+
---
+++ textEditor/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace textEditor
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog ofd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n = 0;
+        int u = 0;
+        int i = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Save";
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt" + "|" +
+                                "Image Files (*.png;*.jpg)|*.png;*.jpg" + "|" +
+                                "All Files (*.*)|*.*";
+            saveFileDialog.DefaultExt = "txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                rtbUpis.Text = saveFileDialog.FileName;
+            }
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ofd = new OpenFileDialog();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+              string text = File.ReadAllText(ofd.FileName);
+                rtbUpis.Text = text;
+            }       
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+        
+                rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Bold);
+            n++;
+            if(n == 2)
+            {
+                rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Regular);
+                n = 0;
+            }
+              
+        }
+
+        private void btnUnder_Click(object sender, EventArgs e)
+        {
+            rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Underline);
+            u++;
+            if (u == 2)
+            {
+                rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Regular);
+                u = 0;
+            }
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Italic);
+            i++;
+            if (i == 2)
+            {
+                rtbUpis.SelectionFont = new Font(rtbUpis.Font, FontStyle.Regular);
+                i = 0;
+            }
+        }
+    }
+}
